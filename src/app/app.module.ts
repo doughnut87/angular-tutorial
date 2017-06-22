b54176667314ddb7a0789d737283c6d5b29456fb
Douglas Hing <doug.hing@enghouse.com>
2017-06-22 16:41:03 +1200

    section 3 of tutorial.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
 
+// Describes the class, (Decorator)
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  imports:      [ BrowserModule ], // what stuff do i need
+  declarations: [ AppComponent ], // what things are in my app
+  bootstrap:    [ AppComponent ] // where to start
 })
-export class AppModule { }
+export class  AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,5 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+// start here --> App Module
 platformBrowserDynamic().bootstrapModule(AppModule);
